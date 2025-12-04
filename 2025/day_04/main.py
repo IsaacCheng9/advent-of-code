771c5568104a3eb366cbc64070bbd6f45888a4ee
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2025-12-04 23:25:10 +0000

    Add day 4 part 1 2025 solution

---
+++ 2025/day_04/main.py
@@ -0,0 +1,90 @@
+import fileinput  # noqa
+import heapq  # noqa
+import math  # noqa
+import os  # noqa
+import re  # noqa
+import sys  # noqa
+from collections import Counter, defaultdict, deque, namedtuple  # noqa
+from functools import cache  # noqa
+from itertools import (  # noqa
+    combinations,
+    combinations_with_replacement,
+    count,
+    permutations,
+    product,
+)
+from bisect import bisect_left, bisect_right  # noqa
+from math import gcd, lcm  # noqa
+from pathlib import Path  # noqa
+from string import ascii_lowercase, ascii_uppercase  # noqa
+
+# Add the parent directory to the PYTHONPATH.
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.utils import (  # noqa
+    calculate_manhattan_distance,
+    execute_with_runtime,
+    parse_2d_grid_strs,
+    parse_input,
+    parse_lines,
+    parse_list_of_ints,
+)
+
+
+def part_one(input_file: str):
+    data = parse_2d_grid_strs(input_file)
+    NUM_ROWS = len(data)
+    NUM_COLS = len(data[0])
+
+    res = 0
+    adj_offsets = [
+        (-1, -1),
+        (-1, 0),
+        (-1, 1),
+        (0, -1),
+        (0, 1),
+        (1, -1),
+        (1, 0),
+        (1, 1),
+    ]
+
+    for row_num, row in enumerate(data):
+        for col_num, val in enumerate(row):
+            if val != "@":
+                continue
+
+            num_adj_rolls = 0
+            for row_diff, col_diff in adj_offsets:
+                new_row = row_num + row_diff
+                new_col = col_num + col_diff
+                if new_row not in range(NUM_ROWS) or new_col not in range(NUM_COLS):
+                    continue
+
+                if data[new_row][new_col] == "@":
+                    num_adj_rolls += 1
+
+            if num_adj_rolls < 4:
+                res += 1
+
+    return res
+
+
+def part_two(input_file: str):
+    data = parse_input(input_file)
+
+
+if __name__ == "__main__":
+    day_path = Path(__file__).parent
+    input_path = day_path / "input.txt"
+
+    print("\n--- Part One ---")
+    answer1, duration1 = execute_with_runtime(part_one, str(input_path))
+    print(f"Answer: {answer1}")
+    print(f"Runtime: {duration1:.4f} milliseconds")
+
+    print("\n--- Part Two ---")
+    answer2, duration2 = execute_with_runtime(part_two, str(input_path))
+    print(f"Answer: {answer2}")
+    print(f"Runtime: {duration2:.4f} milliseconds")
+
+    print(f"\nTotal runtime: {duration1 + duration2:.4f} milliseconds")
