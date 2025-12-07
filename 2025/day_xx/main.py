9a19b0cc4d53f65cdeb009d39be72f4e781ced87
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2025-12-07 02:30:31 +0000

    Add day_xx with template code for 2025

---
+++ 2025/day_xx/main.py
@@ -0,0 +1,59 @@
+import fileinput  # noqa
+import heapq  # noqa
+import io  # noqa
+import math  # noqa
+import numpy as np  # noqa
+import os  # noqa
+import pandas as pd  ## noqa
+import re  # noqa
+import sys  # noqa
+from collections import Counter, defaultdict, deque, namedtuple  # noqa
+from functools import cache  # noqa
+from itertools import (  # noqa
+    combinations,
+    combinations_with_replacement,
+    count,
+    permutations,
+    product,
+)
+from bisect import bisect_left, bisect_right  # noqa
+from math import gcd, lcm  # noqa
+from pathlib import Path  # noqa
+from string import ascii_lowercase, ascii_uppercase  # noqa
+
+# Add the parent directory to the PYTHONPATH.
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.utils import (  # noqa
+    calculate_manhattan_distance,
+    execute_with_runtime,
+    parse_2d_grid_strs,
+    parse_input,
+    parse_lines,
+    parse_list_of_ints,
+)
+
+
+def part_one(file_path: str):
+    data = parse_input(file_path)
+
+
+def part_two(file_path: str):
+    data = parse_input(file_path)
+
+
+if __name__ == "__main__":
+    day_path = Path(__file__).parent
+    input_path = day_path / "input.txt"
+
+    print("\n--- Part One ---")
+    answer1, duration1 = execute_with_runtime(part_one, str(input_path))
+    print(f"Answer: {answer1}")
+    print(f"Runtime: {duration1:.4f} milliseconds")
+
+    print("\n--- Part Two ---")
+    answer2, duration2 = execute_with_runtime(part_two, str(input_path))
+    print(f"Answer: {answer2}")
+    print(f"Runtime: {duration2:.4f} milliseconds")
+
+    print(f"\nTotal runtime: {duration1 + duration2:.4f} milliseconds")
