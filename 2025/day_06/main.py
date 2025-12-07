9c231dd65e058417afc03a369b39c5d4bcca0be2
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2025-12-07 02:27:34 +0000

    Add day 6 part 1 2025 solution

---
+++ 2025/day_06/main.py
@@ -0,0 +1,72 @@
+import fileinput  # noqa
+import heapq  # noqa
+import math  # noqa
+import os  # noqa
+import re  # noqa
+import sys  # noqa
+from collections import Counter, defaultdict, deque, namedtuple  # noqa
+from functools import cache  # noqa
+from itertools import (  # noqa
+    combinations,
+    combinations_with_replacement,
+    count,
+    permutations,
+    product,
+)
+from bisect import bisect_left, bisect_right  # noqa
+from math import gcd, lcm  # noqa
+from pathlib import Path  # noqa
+from string import ascii_lowercase, ascii_uppercase  # noqa
+
+import pandas as pd
+
+# Add the parent directory to the PYTHONPATH.
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.utils import (  # noqa
+    calculate_manhattan_distance,
+    execute_with_runtime,
+    parse_2d_grid_strs,
+    parse_input,
+    parse_lines,
+    parse_list_of_ints,
+)
+
+
+def part_one(input_file: str):
+    data = parse_lines(input_file)
+    data = [line.split() for line in data]
+    df = pd.DataFrame(data)
+    num_cols = df.shape[1]
+
+    total = 0
+    for col in range(num_cols):
+        operator = df.iloc[-1, col]
+        nums = df.iloc[:-1, col].astype(int).to_numpy()
+        if operator == "+":
+            total += nums.sum()
+        elif operator == "*":
+            total += nums.prod()
+
+    return total
+
+
+def part_two(input_file: str):
+    data = parse_input(input_file)
+
+
+if __name__ == "__main__":
+    day_path = Path(__file__).parent
+    input_path = day_path / "input.txt"
+
+    print("\n--- Part One ---")
+    answer1, duration1 = execute_with_runtime(part_one, str(input_path))
+    print(f"Answer: {answer1}")
+    print(f"Runtime: {duration1:.4f} milliseconds")
+
+    print("\n--- Part Two ---")
+    answer2, duration2 = execute_with_runtime(part_two, str(input_path))
+    print(f"Answer: {answer2}")
+    print(f"Runtime: {duration2:.4f} milliseconds")
+
+    print(f"\nTotal runtime: {duration1 + duration2:.4f} milliseconds")
