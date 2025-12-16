957bf6990badc4aa65a4b4ddb66e4ca31ec5ba27
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2025-12-16 13:43:41 +0000

    Add day 11 part 1 2025 solution

---
+++ 2025/day_11/main.py
@@ -0,0 +1,72 @@
+import fileinput  # noqa
+import heapq  # noqa
+import math  # noqa
+import os  # noqa
+import re  # noqa
+import sys  # noqa
+from collections import Counter, defaultdict, deque, namedtuple  # noqa
+from functools import cache  # noqa
+from itertools import (  # noqa
+    combinations,
+    combinations_with_replacement,
+    count,
+    permutations,
+    product,
+)
+from bisect import bisect_left, bisect_right  # noqa
+from math import gcd, lcm  # noqa
+from pathlib import Path  # noqa
+from string import ascii_lowercase, ascii_uppercase  # noqa
+
+from functools import lru_cache
+
+# Add the parent directory to the PYTHONPATH.
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils.utils import (  # noqa
+    calculate_manhattan_distance,
+    execute_with_runtime,
+    parse_2d_grid_strs,
+    parse_input,
+    parse_lines,
+    parse_list_of_ints,
+)
+
+
+def part_one(input_file: str):
+    @lru_cache
+    def dfs(node) -> int:
+        if node == "out":
+            return 1
+        else:
+            return sum(dfs(y) for y in edges[node])
+
+    lines = parse_lines(input_file)
+    edges: dict[str, list[str]] = {}
+
+    for line in lines:
+        x, ys = line.split(": ")
+        ys = ys.split(" ")
+        edges[x] = ys
+
+    return dfs("you")
+
+
+def part_two(input_file: str): ...
+
+
+if __name__ == "__main__":
+    day_path = Path(__file__).parent
+    input_path = day_path / "input.txt"
+
+    print("\n--- Part One ---")
+    answer1, duration1 = execute_with_runtime(part_one, str(input_path))
+    print(f"Answer: {answer1}")
+    print(f"Runtime: {duration1:.4f} milliseconds")
+
+    print("\n--- Part Two ---")
+    answer2, duration2 = execute_with_runtime(part_two, str(input_path))
+    print(f"Answer: {answer2}")
+    print(f"Runtime: {duration2:.4f} milliseconds")
+
+    print(f"\nTotal runtime: {duration1 + duration2:.4f} milliseconds")
